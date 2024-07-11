5eafa599dd92da0842109ee99eca7c66e898a113
Hide-Fun <58735730+Hide-Fun@users.noreply.github.com>
2024-07-11 19:37:02 +0900

    fix

--- R/db_classify_acc.R
+++ R/db_classify_acc.R
@@ -8,7 +8,8 @@ db_classify_acc <- function(.acc) {
   cat(cli::col_br_blue(cli::style_bold("Removing RefSeq IDs.")))
   cat(cli::col_br_blue(cli::style_bold(glue::glue("filtered data: {len} .\n",  len = length(filtered_ids)))))
   dbutilis::acc_table |>
-    mutate(
+    dplyr::filter(!is.na(accession_format)) |>
+    dplyr::mutate(
       query = purrr::map_int(accession_pat, ~sum(stringr::str_detect(filtered_ids, .x)))
     )
 }
