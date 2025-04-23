0a70fab2bd36247c238550ceb05c82754e93523e
Hide-Fun <58735730+Hide-Fun@users.noreply.github.com>
2025-04-23 18:55:06 +0900

    Refactor db_get_fasta and db_get_meta functions: replace promap2_chr with purrr::map2_chr. Update db_parse_xml to enhance XML parsing and remove deprecated functions. Clean up spacing and formatting for better code clarity.

--- R/db_get_fasta.R
+++ R/db_get_fasta.R
@@ -9,30 +9,30 @@
 db_get_fasta <- function(.acc, .dest_dir, .n_max, .sleep, .try_max = NULL) {
   acc <- .acc
   # if dose not exist, create save directory.
-  if(fs::dir_exists(here::here(.dest_dir)) == FALSE) {
+  if (fs::dir_exists(here::here(.dest_dir)) == FALSE) {
     fs::dir_create(here::here(.dest_dir))
   }
   # split accession ids.
-  acc_list <- split(acc, ceiling(seq_along(acc)/.n_max))
+  acc_list <- split(acc, ceiling(seq_along(acc) / .n_max))
   # accession id
   .ids <- purrr::map(acc_list, paste0, collapse = ",")
   # set query.
   base_url <- "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id={.ids}&rettype=fasta&retmode=text"
-  use_urls <- unlist(purrr::map(base_url, ~glue::glue(.x, .ids = .ids)))
-  path_list <- unlist(purrr::map(1:length(use_urls), ~paste0(.dest_dir, "/", .x, ".fasta")))
+  use_urls <- unlist(purrr::map(base_url, ~ glue::glue(.x, .ids = .ids)))
+  path_list <- unlist(purrr::map(1:length(use_urls), ~ paste0(.dest_dir, "/", .x, ".fasta")))
   # download.
-  res <- promap2_chr(use_urls, path_list, ~curl_download2(.x, .y, .sleep = .sleep))
+  res <- purrr::map2_chr(use_urls, path_list, ~ curl_download2(.x, .y, .sleep = .sleep))
   # trials.
-  if(!is.null(.try_max)) {
+  if (!is.null(.try_max)) {
     # make empty list.
     res_list <- vector(mode = "list", length = .try_max + 1)
     res_list[[1]] <- res
     for (i in 1:.try_max) {
-      if(length(which(res_list[[i]] == "fault")) != 0) {
+      if (length(which(res_list[[i]] == "fault")) != 0) {
         # update queries.
         use_urls <- use_urls[which(res_list[[i]] == "fault")]
         path_list <- path_list[which(res_list[[i]] == "fault")]
-        res_list[[i + 1]] <- promap2_chr(use_urls, path_list, ~curl_download2(.x, .y, .sleep = .sleep))
+        res_list[[i + 1]] <- purrr::map2_chr(use_urls, path_list, ~ curl_download2(.x, .y, .sleep = .sleep))
         res <- res_list[[i + 1]]
       } else {
         res <- res_list[[i]]
--- R/db_parse_xml.R
+++ R/db_parse_xml.R
@@ -6,110 +6,116 @@
 #' @export
 db_parse_gbxml <- function(.target_dir, .gbseq, .workers) {
   # set multi session.
-  future::plan(future::multisession, workers = .workers)
+  mirai::daemons(.workers)
   # get xml path.
   cat(cli::col_br_blue(cli::style_bold("parse XML to csv.\n")))
   xml_path <- fs::dir_ls(here::here(.target_dir), glob = "*.xml")
   cat(cli::col_br_blue(cli::style_bold(glue::glue("parse {len} xml files.\n", len = length(xml_path)))))
   # load  & parse xml.
-  parsed <- suppressWarnings(furrr::future_map(
+  parsed <- purrr::map(
     xml_path,
     parse_gbxml2,
-    .gbseq = .gbseq,
-    .progress = TRUE
-  ))
+    .progress = TRUE,
+    .parallel = TRUE
+  )
+  # reset multi session.
+  mirai::daemons(0)
   return(parsed)
 }
 
-parse_gbxml2 <- function(.path, .gbseq) {
+parse_gbxml2 <- function(.path) {
   xml <- xml2::read_xml(.path)
-  parsed <- parse_gbxml(.xml = xml, .gbseq = .gbseq)
+  parsed <- parse_gbxml(.xml = xml)
   return(parsed)
 }
 
-parse_gbxml <- function(.xml, .gbseq = c("all", "non_seq", "manual")) {
+parse_gbxml <- function(.xml) {
   # get xml size.
   size <- xml2::xml_length(.xml)
   # get children.
   list <- xml2::as_list(.xml)
   # split xml.
   # each GBSeq stored in list.
-  gbseq_list <- supmap(1:size, ~SplitGBset(.list = list, .num = .x))
-  feature_table_list <- supmap(gbseq_list, purrr::pluck, "GBSeq_feature-table", "GBFeature", "GBFeature_quals")
-  other_seqids_list <- supmap(gbseq_list, purrr::pluck, "GBSeq_other-seqids")
-  references_list <- supmap(gbseq_list, purrr::pluck, "GBSeq_references", "GBReference")
+  gbseq_list <- purrr::map(1:size, ~ SplitGBset(.list = list, .num = .x))
+  feature_table_list <- purrr::map(gbseq_list, purrr::pluck, "GBSeq_feature-table", "GBFeature", "GBFeature_quals")
+  other_seqids_list <- purrr::map(gbseq_list, purrr::pluck, "GBSeq_other-seqids")
+  references_list <- purrr::map(gbseq_list, purrr::pluck, "GBSeq_references", "GBReference")
 
   # parse gbseq_list.
-  if(.gbseq == "all") {
-    lev_gbeq <- list("GBSeq_locus",
-                     "GBSeq_length",
-                     "GBSeq_strandedness",
-                     "GBSeq_moltype",
-                     "GBSeq_topology",
-                     "GBSeq_division",
-                     "GBSeq_update-date",
-                     "GBSeq_create-date",
-                     "GBSeq_definition",
-                     "GBSeq_primary-accession",
-                     "GBSeq_accession-version",
-                     "GBSeq_source",
-                     "GBSeq_organism",
-                     "GBSeq_taxonomy",
-                     "GBSeq_sequence")
-  } else if(.gbseq == "non_seq") {
-    lev_gbeq <- list("GBSeq_locus",
-                     "GBSeq_length",
-                     "GBSeq_strandedness",
-                     "GBSeq_moltype",
-                     "GBSeq_topology",
-                     "GBSeq_division",
-                     "GBSeq_update-date",
-                     "GBSeq_create-date",
-                     "GBSeq_definition",
-                     "GBSeq_primary-accession",
-                     "GBSeq_accession-version",
-                     "GBSeq_source",
-                     "GBSeq_organism",
-                     "GBSeq_taxonomy")
-  } else if(.gbseq == "manual") {
-    stop("not implemented now")
-  }
+  lev_gbeq <- list(
+    "GBSeq_locus",
+    "GBSeq_length",
+    "GBSeq_strandedness",
+    "GBSeq_moltype",
+    "GBSeq_topology",
+    "GBSeq_division",
+    "GBSeq_update-date",
+    "GBSeq_create-date",
+    "GBSeq_definition",
+    "GBSeq_primary-accession",
+    "GBSeq_accession-version",
+    "GBSeq_source",
+    "GBSeq_organism",
+    "GBSeq_taxonomy",
+    "GBSeq_sequence"
+  )
+
   # get lev_gbseq.
   repl_gbseq <- PatColnames(.x = lev_gbeq)
-  gbseq <- supmap_dfr(gbseq_list, MapGetValue, .names = lev_gbeq) %>%
-    dplyr::rename_with(stringr::str_replace_all,
-                       .cols = dplyr::everything(),
-                       pattern = repl_gbseq)
+  gbseq <- purrr::map(gbseq_list, MapGetValue, .names = lev_gbeq) %>%
+    purrr::list_rbind() %>%
+    dplyr::rename_with(
+      .fn = stringr::str_replace_all,
+      .cols = dplyr::everything(),
+      pattern = repl_gbseq
+    )
 
   # parse references_list.
-  lev_ref <- list("GBReference_title",
-                  "GBReference_journal")
+  lev_ref <- list(
+    "GBReference_title",
+    "GBReference_journal"
+  )
   repl_ref <- PatColnames(.x = lev_ref)
-  references <- supmap_dfr(references_list, MapGetValue, .names = lev_ref) %>%
-    dplyr::rename_with(stringr::str_replace_all,
-                       .cols = dplyr::everything(),
-                       pattern = repl_ref)
+  references <- purrr::map(references_list, MapGetValue, .names = lev_ref) %>%
+    purrr::list_rbind() %>%
+    dplyr::rename_with(
+      .fn = stringr::str_replace_all,
+      .cols = dplyr::everything(),
+      pattern = repl_ref
+    )
 
   # parse feature_table_list.
   feature_table <- feature_table_list %>%
     tibble::enframe(name = "id") %>%
     tidyr::unnest(value) %>%
     dplyr::mutate(name = names(value)) %>%
-    dplyr::mutate(name2 = supmap(value, names),
-                  value2 = supmap(value, unlist)) %>%
+    dplyr::mutate(
+      name2 = supmap(value, names),
+      value2 = supmap(value, unlist)
+    ) %>%
     tidyr::unnest(c(name2, value2)) %>%
-    tidyr::pivot_wider(names_from = "name2", values_from = "value2") %>%
-    dplyr::mutate(GBQualifier_value = dplyr::if_else(
-      is.na(GBQualifier_value), GBQualifier_name, GBQualifier_value
-    )) %>%
+    tidyr::pivot_wider(
+      names_from = "name2",
+      values_from = "value2"
+    ) %>%
+    dplyr::mutate(
+      GBQualifier_value = dplyr::if_else(
+        is.na(GBQualifier_value), GBQualifier_name, GBQualifier_value
+      )
+    ) %>%
     dplyr::select(id, GBQualifier_name, GBQualifier_value) %>%
-    tidyr::pivot_wider(names_from = "GBQualifier_name", values_from = "GBQualifier_value")
+    tidyr::pivot_wider(
+      names_from = "GBQualifier_name",
+      values_from = "GBQualifier_value"
+    )
 
   # parse other_seqids_list.
   other_seqids <- other_seqids_list %>%
     tibble::enframe(name = "id") %>%
-    dplyr::mutate(value2 = supmap(value, unlist),
-                  value2 = supmap(value2, paste0, collapse = "_")) %>%
+    dplyr::mutate(
+      value2 = purrr::map(value, unlist),
+      value2 = purrr::map(value2, paste0, collapse = "_")
+    ) %>%
     tibble::add_column(name2 = "GBSeqid") %>%
     tidyr::unnest(c(name2, value2)) %>%
     dplyr::select(id, name2, value2) %>%
@@ -117,7 +123,7 @@ parse_gbxml <- function(.xml, .gbseq = c("all", "non_seq", "manual")) {
     dplyr::select(-id)
 
   # check row number.
-  if(nrow(gbseq) != size | nrow(references) != size | nrow(feature_table) != size | nrow(other_seqids) != size) {
+  if (nrow(gbseq) != size | nrow(references) != size | nrow(feature_table) != size | nrow(other_seqids) != size) {
     stop("row number is not correct")
   }
   # bind all data.
@@ -141,14 +147,14 @@ SplitGBset <- function(.list, .num) {
 # get xml text value.
 GetValue <- function(.list, .name) {
   rlt <- unlist(.list[[.name]])
-  if(is.null(rlt)) {
+  if (is.null(rlt)) {
     rlt <- NA
   }
   return(rlt)
 }
 
 # multiple name.
-MapGetValue <- function( .list, .names, .f = GetValue) {
+MapGetValue <- function(.list, .names, .f = GetValue) {
   supmap_dfc(.names, GetValue, .list = .list)
 }
 
@@ -156,7 +162,8 @@ MapGetValue <- function( .list, .names, .f = GetValue) {
 PatColnames <- function(.x) {
   len <- length(.x)
   colname <- paste0("...", rev(1:len))
-  rlang::set_names(nm = colname,
-                   x = rev(unlist(.x)))
+  rlang::set_names(
+    nm = colname,
+    x = rev(unlist(.x))
+  )
 }
-
